a4d140db4c4f200ded54a0b9b84db574d4dfeff2
Pauline <mya@mya-ow.net>
2018-07-25 18:16:54 +0200

    ajout initial du projet

---
+++ db/migrate/20180725160244_add_likesto_article.rb
@@ -0,0 +1,5 @@
+class AddLikestoArticle < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :likes, :article, foreign_key: true
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# 10.times do
+#   Article.create!(title: Faker::Pokemon.name, content: Faker::Lorem.sentence, user_id: rand(1..10))
+# end
+
+# 10.times do
+#   User.create!(first_name: Faker::Overwatch.hero, last_name: Faker::Overwatch.location, email: Faker::Internet.email)
+# end
+
+# 5.times do
+#   Category.create!(name: Faker::Cat.name)
+# end
+
+# Article.all.each do |article|
+#   article.update(category_id: rand(1..5))
+#  end
+
+# 15.times do
+#   Comment.create!(content: Faker::Lorem.sentence, article_id: rand(1..11), user_id: rand(1..11))
+# end
+
+15.times do
+  Like.create!(article_id: rand(1..11), user_id: rand(1..11))
+end

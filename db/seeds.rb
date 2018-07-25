2fa6e095fbaeca10a73228d273babd2c2152d157
Pauline <mya@mya-ow.net>
2018-07-25 18:36:19 +0200

    modif seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-# 10.times do
-#   Article.create!(title: Faker::Pokemon.name, content: Faker::Lorem.sentence, user_id: rand(1..10))
-# end
+10.times do
+  Article.create!(title: Faker::Pokemon.name, content: Faker::Lorem.sentence, user_id: rand(1..10))
+end
 
-# 10.times do
-#   User.create!(first_name: Faker::Overwatch.hero, last_name: Faker::Overwatch.location, email: Faker::Internet.email)
-# end
+10.times do
+  User.create!(first_name: Faker::Overwatch.hero, last_name: Faker::Overwatch.location, email: Faker::Internet.email)
+end
 
-# 5.times do
-#   Category.create!(name: Faker::Cat.name)
-# end
+5.times do
+  Category.create!(name: Faker::Cat.name)
+end
 
-# Article.all.each do |article|
-#   article.update(category_id: rand(1..5))
-#  end
+Article.all.each do |article| # ajoute une category_id à chaque article
+  article.update(category_id: rand(1..5))
+ end
 
-# 15.times do
-#   Comment.create!(content: Faker::Lorem.sentence, article_id: rand(1..11), user_id: rand(1..11))
-# end
+15.times do
+  Comment.create!(content: Faker::Lorem.sentence, article_id: rand(1..11), user_id: rand(1..11))
+end
 
 15.times do
   Like.create!(article_id: rand(1..11), user_id: rand(1..11))
